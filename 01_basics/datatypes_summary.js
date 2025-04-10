ef77739bef64532ebc63520e6dbefbcb56836e9d
Abhishek puri goswami <nicky871782@gmail.com>
2025-04-10 17:03:42 +0000

    datatypes_summary.js/03_conversionOperation.js/04_comparision.js

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,57 @@
+ //   #primetive types 
+
+ // 7 types :-
+ // string,
+ // number,
+ // boolearn,
+ // null, 
+ // undefined,
+ // Symbol
+ // BigInt
+
+
+ const score = 100
+ const scoreValeue = 100.3
+
+
+ const isLoggedIn = false
+ const outsideTemp = null
+ let userEmail;
+
+ const id = Symbol('123')
+ const anotherId = Symbol('123')
+
+ console.log(id === anotherId )
+
+ const bigNumber = 9454896464654684658468n
+ 
+
+
+// Reference (Non primitive)
+
+
+// arry,
+// objects,
+// Functions,
+
+//arry
+const heros = ["shaktiman", "naagraj", "doga"]
+
+//objects
+let myObj ={
+ name: "htesha",
+ age: 25,
+}
+
+//function
+const myFunction = function (){
+
+console.log("hello world");
+}
+
+console.log(typeof outsideTemp);
+
+console.log(typeof scoreValeue)
+
+console.log(typeof myFunction);
+
