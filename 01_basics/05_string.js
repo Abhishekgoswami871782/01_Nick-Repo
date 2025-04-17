c07855eb1909b9b1bc81acccb558438c3612e31a
Abhishek puri goswami <nicky871782@gmail.com>
2025-04-17 14:36:36 +0000

    array silice, splice

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -1,1 +1,26 @@
-console.log ("hello word")
+const name = "hitesh"
+const repoCount = 50 
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Abhishek-in-the-bhopal')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase())
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const newStringOne = "     Abhishek     "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+console.log(url.replace('%20', '-'))
+
+
+
+console.log(gameName.split('-'));
---
+++ 01_basics/07_datetime.js
@@ -0,0 +1,29 @@
+// Dates
+
+
+
+// let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// console.log(myCreatedDate.toLocaleString());
+
+ let myCreatedDate = new Date("2023-01-14")
+// console.log(myCreatedDate.toLocaleString());
+
+// let myTimeStamp = Date.now()
+
+// console.log(myCreatedDate.getTime());
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth());
+// console.log(newDate.getDay());
+// console.log(newDate.getTime());
+
+newDate. toLocaleString('default', {
+    weekday: "long"
+})
